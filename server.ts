4af53037000ca68a6ad980071317a3d38b670d99
bestauffer <77029920+bestauffer@users.noreply.github.com>
2023-06-04 00:43:37 -0700

    Create server.ts

---
+++ server.ts
@@ -0,0 +1,15 @@
+import { serve } from "https://deno.land/std@0.184.0/http/server.ts";
+const BOOK_ROUTE = new URLPattern({ pathname: "/books/:id" });
+
+function handler(req: Request): Response {
+  const match = BOOK_ROUTE.exec(req.url);
+  if (match) {
+    const id = match.pathname.groups.id;
+    return new Response(`Book ${id}`);
+  }
+  return new Response("Not found (try /books/1)", {
+    status: 404,
+  });
+}
+console.log("Listening on http://localhost:8000");
+serve(handler);

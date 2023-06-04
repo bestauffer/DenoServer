13462af665ba438528fae5e0fd9cc9419d541b2b
bestauffer <77029920+bestauffer@users.noreply.github.com>
2023-06-04 01:54:39 -0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,15 +1,10 @@
 import { serve } from "https://deno.land/std@0.184.0/http/server.ts";
-const BOOK_ROUTE = new URLPattern({ pathname: "/books/:id" });
+//const BOOK_ROUTE = new URLPattern({ pathname: "/books/:id" });
 
-function handler(req: Request): Response {
-  const match = BOOK_ROUTE.exec(req.url);
-  if (match) {
-    const id = match.pathname.groups.id;
-    return new Response(`Book ${id}`);
-  }
-  return new Response("Not found (try /books/1)", {
-    status: 404,
-  });
+function handler(): Response {
+
+    return new Response(`Hello`);
+
 }
 console.log("Listening on http://localhost:8000");
 serve(handler);
